f800e9084e1c5ac44800d41a0c8ad35a67f5bf10
44David <david.soane@outlook.com>
2024-11-02 12:56:24 -0600

    Created SQL database and set up configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
     
       <div className="float-left relative border-r h-screen pr-11">
-        
+
       </div>
 
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">  
@@ -13,4 +13,5 @@ export default function Home() {
       </div>
     </>
   );
+
 }
