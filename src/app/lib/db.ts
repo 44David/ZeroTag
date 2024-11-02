fd70e379acb359020e7f4bff5f2fc1dd2974e896
44David <david.soane@outlook.com>
2024-11-02 15:42:59 -0600

    Changed SQL configuration

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,12 +1,17 @@
-import knex from 'knex';
-
-export const db = knex ({
-    client: "mysql",
-    connection: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER, 
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        
-    },
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
+
+export default connection
+
+connection.connect();
+
+//@ts-ignore
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('Solution is: ', results[0].solution);
+})
