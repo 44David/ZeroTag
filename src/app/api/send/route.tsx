1ac247506df500a340670afbd3c46972bea218f2
44David <david.soane@outlook.com>
2024-11-05 09:38:40 -0600

    Send SQL data through api route from form

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -1,9 +1,17 @@
+import pool from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse){
-    const data = await req.formData()
-    
+    const data = await (await req.formData()).get("username")
+
     console.log("Req.body", data)
+
+    const query = pool.query('INSERT INTO test_table VALUES (?)', data, function (err, result, fields) {
+        if (err) throw err;
+    })
+
+    console.log(query.sql)
+
 
     return NextResponse.json({"Hello": "Hello"})
 }
