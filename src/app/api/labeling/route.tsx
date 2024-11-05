c11874bfffee4c616fd4e80ec8ea11b76e62f2a8
44David <david.soane@outlook.com>
2024-11-04 19:42:12 -0600

    Created page to send data

--- src/app/api/labeling/route.tsx
+++ src/app/api/labeling/route.tsx
@@ -2,35 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 import pool from "@/app/lib/db"
 
 
-
-export async function GET() {
-    // await  pool.query('SELECT username FROM test_table', function (err, result, fields) {
-    //     if (err) throw err;
-    //     return NextResponse.json({'returned query': result})
-   
-    //@ts-ignore
-    function get_query(callback){
-        pool.query('SELECT username FROM test_table', function (err, result) {
+async function get_query(callback:any){
+        await pool.query('SELECT username FROM test_table', function (err:any, result:any) {
             if (err) {
                 throw err;
             } 
-            res = result[0]
             return callback(result[0]);
         })
-    }
+}
 
-    let res = '';
-    let res_query
+export async function GET() {
+    var res = ''
 
-    get_query(res_query = (result) => {
-        res = result
+    console.log(get_query(res))
 
-    })
-
-    console.log('Outside of callback', res)
-
-
-    return NextResponse.json({"Hello":"Hello"})
+    return NextResponse.json({"test": "test"})
 
 
 
---
+++ src/app/send/page.tsx
@@ -0,0 +1,5 @@
+export default function Send() {
+    return (
+        <h1>Hello</h1>
+    )
+}
