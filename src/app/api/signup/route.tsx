960907db8fdd9e74d9c014d4ff2b9a66ae106c16
44David <david.soane@outlook.com>
2024-11-12 09:51:45 -0600

    Create JWT for authentication

--- src/app/api/signup/route.tsx
+++ src/app/api/signup/route.tsx
@@ -2,12 +2,15 @@ import pool from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 const bcrypt = require('bcrypt')
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 // bcrypt variables 
 const saltRounds = 10;
 
 export async function POST(req: NextRequest, res: NextResponse) {
     
+    
+
     const formData = await req.formData();
 
     const password = formData.get("password");
@@ -16,18 +19,25 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
     if (validator.isEmail(email)) {
 
-        bcrypt.genSalt(saltRounds, function(err:any, salt:any) {
-                
+        bcrypt.genSalt(saltRounds, function(err:any, salt:any) {  
             bcrypt.hash(password, salt, function(err:any, hash:any) {
 
                 pool.query("INSERT INTO users (email_address, password) VALUES (?, ?)", [email, hash]);
 
             })
-        })
+        });
+
+        const jwtToken = jwt.sign({email}, 'privatekey', { expiresIn: '1h'}, (err:any, token:any) => {
+            if (err) {
+                console.log(err)
+            }
+            return token;
+        })      
         
+        console.log(jwtToken)
         return NextResponse.json({ "message": "Signed up successfully" }, { status: 200 })
 
-    }  {
+    }  else {
 
         return NextResponse.json({ error: "Not a valid email address." }, { status: 500 })
     };
