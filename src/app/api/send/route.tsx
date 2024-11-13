5effe2de09b7f222d927ea5bc03a9f34c7e688bc
44David <david.soane@outlook.com>
2024-11-13 09:24:14 -0600

    Attempt to use session cookie value for SQL queries

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -1,6 +1,7 @@
 import pool from "@/app/lib/db";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-const jwt = require('jsonwebtoken');
+
 
 export async function POST(req: NextRequest, res: NextResponse) {
 
@@ -9,10 +10,25 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
     const inputLabels = formData.get("input-labels")
 
+    const cookieStore = await cookies();
+    const sessionValue = cookieStore.get('session')?.value;
+    
+    console.log(sessionValue)
+
+
+    const [rows] = await pool.query('SELECT * FROM users WHERE session_value=(?)', sessionValue);
+
+    //@ts-ignore
+    const [row] = rows;
+    const queryEmail = row.email_address;
+
+    console.log(queryEmail)
+
 
     try {
         pool.query("INSERT INTO labels (input_labels) VALUES (?)", inputLabels)
         return NextResponse.json({"message": "Success"}, { status: 200 })
+
     } catch (err:any) {
         return NextResponse.json({ error: "Could not insert input label values" }, {status: 500})
     }
