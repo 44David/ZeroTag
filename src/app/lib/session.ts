3365fefcc11310f73f9897c7257e7ce87b3a3872
44David <david.soane@outlook.com>
2024-11-14 10:09:24 -0600

    Added error handling for session cookie and render email in navbar

--- src/app/lib/session.ts
+++ src/app/lib/session.ts
@@ -3,16 +3,19 @@ import { cookies } from "next/headers";
 
 export async function getEmail() {
     
-    const cookieStore = await cookies();
+    try { 
+        const cookieStore = await cookies();
 
-    const sessionValue = cookieStore.get('session')?.value;
-    
-    const [rows] = await pool.query('SELECT * FROM users WHERE session_value=(?)', sessionValue);
+        const sessionValue = cookieStore.get('session')?.value;
+        
+        const [rows] = await pool.query('SELECT * FROM users WHERE session_value=(?)', sessionValue);
 
-    //@ts-ignore
-    const [row] = rows;
-    const queryEmail = row.email_address;
+        //@ts-ignore
+        const [row] = rows;
+        const queryEmail = row.email_address;
 
-    return queryEmail
-
+        return queryEmail
+    } catch (error) {
+        return "Not Signed In"
+    }
 }
