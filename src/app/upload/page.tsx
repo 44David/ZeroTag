8eabfe62cc9432e925e2bb7a15982b155948906f
44David <david.soane@outlook.com>
2024-12-15 14:47:29 -0600

    Added AWS SDK

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -7,19 +7,13 @@ export default function Upload() {
     const [isChecked, setIsChecked] = useState(false);
     const [file, setFile] = useState('');
 
-    async function handleChange(event:any) {
-        setFile(URL.createObjectURL(event.target.files[0]))
-    }
-
     async function onSubmit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
-
-        s3Upload();
 
         const formData = new FormData(e.currentTarget);
         console.log('formData', formData)
 
-        const response = await fetch('http://localhost:3000/api/send', {
+        const response = await fetch('http://localhost:3000/api/upload', {
             method: 'POST',
             body: formData,
         });
@@ -28,44 +22,38 @@ export default function Upload() {
         
     };
 
-    async function s3Upload() {
-        
-    }
-
-
+    async function handleChange(event:any) {
+        setFile(URL.createObjectURL(event.target.files[0]))
+    };
 
     return (
         <form onSubmit={onSubmit}>
-            <fieldset>
 
-                <legend>Submit Images</legend>
+            <input 
+                type="text" 
+                name="input-labels"
+                placeholder="Input Labels"  
+                id="input-labels"
+                disabled={isChecked}
+                className="block"
+            />
 
-                <input 
-                    type="text" 
-                    name="input-labels"
-                    placeholder="Input Labels"  
-                    id="input-labels"
-                    disabled={isChecked}
-                    className="block"
-                />
+            <input 
+                type='checkbox'
+                value="auto-label"
+                name="box"
+                id="box"
+                onChange={(e) => setIsChecked(e.target.checked)}
+            />
 
-                <input 
-                    type='checkbox'
-                    value="auto-label"
-                    name="box"
-                    id="box"
-                    onChange={(e) => setIsChecked(e.target.checked)}
-                />
+            <label htmlFor="box">Automatically create labels?</label>
 
-                <label htmlFor="box">Automatically create labels?</label>
+            <input type="file" className="block" onChange={handleChange}/>
 
-                <input type="file" className="block" onChange={handleChange}/>
+            <Image src={file} width={250} height={500} alt="User provided image"/>
 
-                <Image src={file} width={250} height={500} alt="User provided image"/>
+            <button type="submit" className="block">Submit</button>
 
-                <button type="submit" className="block">Submit</button>
-
-            </fieldset>
 
         </form>
     )
